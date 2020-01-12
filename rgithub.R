6d63674009fbb6f5f07d36d47b57d264bc174ddd
Tim <timschngns@aol.com>
2020-01-12 10:33:00 -0500

    Next commit

--- rgithub.R
+++ rgithub.R
@@ -1,12 +1,17 @@
 # DON'T FORGET TO PULL!
 # PACKAGES ##############################################################################
 install.packages("ISLR")
+install.packages("usethis")
 
 # GO #############################################################################
 library(ISLR)
 data1 = iris
 head(data1)
 
-# New Test line
+library(usethis)
+?use_github
+use_git(message="test")
+edit_r_environ()
+GITHUB_PAT = '3844f5ed1be262b6b5e5e49a0bc81e0c2563e569'
 
 # GO TO TOOLS/VERSION CONTROL/COMMIT/PUSH

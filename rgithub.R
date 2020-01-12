dcfe6375743619610a62386dda22aff452c3f2b1
Tim <timschngns@aol.com>
2020-01-12 09:10:59 -0500

    first commit

---
+++ rgithub.R
@@ -0,0 +1,12 @@
+# DON'T FORGET TO PULL!
+# PACKAGES ##############################################################################
+install.packages("ISLR")
+
+# GO #############################################################################
+library(ISLR)
+data1 = iris
+head(data1)
+
+# New Test line
+
+# GO TO TOOLS/VERSION CONTROL/COMMIT/PUSH
